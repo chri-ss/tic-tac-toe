d2cc65d8fc77e265b9e926623a94ba207394a0e7
Chris Cresswell <christorper@gmail.com>
2021-11-09 23:07:42 -0800

    Add input form and add method for starting game

--- script.js
+++ script.js
@@ -2,31 +2,53 @@ const gameBoard = (() => {
     let board = ['', '', '', '', '', '', '', '', '']
 
     const updateBoard = () => {
-        for(i = 0; i < board.length; ++i)
-        {
+        for (i = 0; i < board.length; ++i) {
             board[i] = 'X';
         }
         return board;
     }
+
+    return { updateBoard};
+})();
+
+const game = (() => {
+    const startGame = () => {
+        let p1 = document.querySelector('.player-one');
+        let p2 = document.querySelector('.player-two');
+        player1 = createPlayer(p1.textContent, 'X');
+        player2 = createPlayer(p2.textContent, 'O');
+
+        console.log(player1, player2);
+    }
+    return {startGame}
+})();
+
+const createPlayer = (name, team) => {
+
+    return { name, team }
+}
+
+const displayController = (() => {
 
     const buildBoard = () => {
         const boardContainer = document.querySelector('.board-container');
         const grid = document.createElement('div');
         grid.classList.add('grid');
         boardContainer.appendChild(grid);
-        for(let i = 0; i < board.length; ++i)
-        {
+        for (let i = 0; i < 9; ++i) {
             const square = document.createElement('div');
             square.classList.add('square');
-            square.textContent = board[i];
             grid.appendChild(square);
         }
     }
 
-    return {updateBoard, buildBoard};
-})();
+    const initNewGame = () => {
+        let start = document.querySelector('.new-game');
+        start.addEventListener('click', (e) => {
+            e.preventDefault();
+            game.startGame();
+        })
+    }
 
-const createPlayer = (name) => {
-    let score = 0;
-    return {score, name}
-}
+    return {buildBoard, initNewGame}
+})();
