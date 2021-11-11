2b4d658cc2c55e52fb1d1fffe7eb152446686642
Chris Cresswell <christorper@gmail.com>
2021-11-11 02:19:47 -0800

    Add function to add x's and o's to the game board grid

--- script.js
+++ script.js
@@ -7,8 +7,28 @@ const gameBoard = (() => {
         }
         return board;
     }
+    
+    const addMarker = () => {
+        let gridCells = Array.from(document.querySelectorAll('.square'));
+        gridCells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                if (player1.turn === true)
+                {
+                    cell.textContent = player1.team;
+                    player1.turn = false;
+                    player2.turn = true;
+                }
+                else if (player2.turn === true)
+                {
+                    cell.textContent = player2.team;
+                    player2.turn = false;
+                    player1.turn = true; 
+                }
+            })
+        })
+    }
 
-    return { updateBoard};
+    return { updateBoard, addMarker};
 })();
 
 const game = (() => {
@@ -17,10 +37,15 @@ const game = (() => {
         let p2 = document.querySelector('.player-two');
         player1 = createPlayer(p1.value, 'X');
         player2 = createPlayer(p2.value, 'O');
+        player1.turn = true;
+        player2.turn = false;
         displayController.buildBoard();
+        displayController.removeForm();
 
-        console.log(player1, player2);
+        return {player1, player2}
     }
+
+    
     return {startGame}
 })();
 
@@ -30,6 +55,14 @@ const createPlayer = (name, team) => {
 }
 
 const displayController = (() => {
+
+    const initNewGame = () => {
+        let start = document.querySelector('.new-game');
+        start.addEventListener('click', (e) => {
+            e.preventDefault();
+            game.startGame();
+        })
+    }
 
     const buildBoard = () => {
         const boardContainer = document.querySelector('.board-container');
@@ -41,14 +74,6 @@ const displayController = (() => {
             square.classList.add('square');
             grid.appendChild(square);
         }
-    }
-
-    const initNewGame = () => {
-        let start = document.querySelector('.new-game');
-        start.addEventListener('click', (e) => {
-            e.preventDefault();
-            game.startGame();
-        })
     }
 
     const removeForm = () => {
@@ -63,5 +88,7 @@ const displayController = (() => {
         form.classList.remove('hide-form');
     }
 
-    return {buildBoard, removeForm, addForm}
+    return {buildBoard, initNewGame, removeForm, addForm}
 })();
+
+displayController.initNewGame();
